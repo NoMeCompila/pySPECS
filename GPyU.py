19e42565f49fed6b7782fd5a421ee024a1a2feb5
Fernando Caballero <cabafer1@gmail.com>
2023-03-16 00:36:05 -0300

    add pc name and ip info

--- GPyU.py
+++ GPyU.py
@@ -1,8 +1,14 @@
 from tabulate import tabulate
 import GPUtil
+import socket 
+import urllib.request
+
+def print_title(title: str) -> None:
+    print("="*50, title, "="*50)
+
 
 def print_cpu_info():
-    print("="*40, "GPU Details", "="*40)
+    print_title('GPU DETAILS')
     gpus = GPUtil.getGPUs()
     list_gpus = []
     for gpu in gpus:
@@ -23,25 +29,38 @@ def print_cpu_info():
         gpu_uuid = gpu.uuid
         list_gpus.append([gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory, gpu_total_memory, gpu_temperature])#, gpu_uuid])
 
-        head = ("id", "nombre", "carga", "memoria libre", "memoria usada", "memoria total", "temperatura")#, "uuid")
+    head = ("id", "nombre", "carga", "memoria libre", "memoria usada", "memoria total", "temperatura")#, "uuid")
     print(tabulate(list_gpus, 
                 headers=head,
                 tablefmt='fancy_grid',
                 stralign='center',
                 floatfmt='.0f'))
 
+
+
+def get_pc_name():
+    # concatenar los archivos con el nombre de la pc
+    return str(socket.gethostname())
+
+# Funcion para obtener las IP
+def get_private_ip():
+    pc_name = socket.gethostname()
+    return str(socket.gethostbyname(pc_name))
+
+def get_public_ip():
+    return str(urllib.request.urlopen('https://ident.me').read().decode('utf8'))
+      
+def print_name_ip_table():
+    print_title('IP INFO')
+    ip_info_list = [[get_pc_name(), get_private_ip(), get_public_ip()]]
+    head = ('Nombre PC', 'IP privada', 'IP Publica')
+    
+    print(tabulate(ip_info_list, 
+    headers=head, 
+    tablefmt='fancy_grid', 
+    stralign='center', 
+    floatfmt='.0f'))
+
 if __name__ == '__main__':
     print_cpu_info()
-
-
-# rios3 = [['Río', 'Long. (Km.)'],
-#          ['Almanzora', 105],
-#          ['Guadiaro', 79],
-#          ['Guadalhorce', 154],
-#          ['Guadalmedina', 51.5]]
-
-# print(tabulate(rios3,
-#                headers='firstrow',
-#                tablefmt='fancy_grid',
-#                stralign='center',
-#                floatfmt='.0f'))
+    print_name_ip_table()
